63d4d1f0e2f7a7dab74720ce07e47aa31a604947
Pheo <CJM1993@gmail.com>
2013-09-05 21:25:46 +1000

    Added more models

--- vmServer/ads/urls.py
+++ vmServer/ads/urls.py
@@ -6,6 +6,6 @@ urlpatterns = patterns ('',
 	# ex: /
 	url(r'^$', views.index, name='index'),
 	url(r'^(?P<ad_id>\d+)/$', views.display, name='display'),	
-	url(r'^authenticate/', 
+	url(r'^authenticate/$', 
 		views.authenticate, name='authenticate')
 )
--- vmServer/ads/views.py
+++ vmServer/ads/views.py
@@ -10,9 +10,9 @@ def display(request,ad_id):
 	return HttpResponse("Message of Ad %s is being displayed." % ad_id)
 
 def authenticate(request):
-	if (request.method == "GET") and ('pi_name' and 'uniq_id' in request.GET):
-		pi_name	= request.GET['pi_name']
-		uniq_id	= request.GET['uniq_id']
+	if request.method == "GET":
+		pi_name	= request.GET.get('pi_name','')
+		uniq_id	= request.GET.get('uniq_id','')
 		Authentications = Authentication.objects.all()
 		for auth in Authentications:
 			if pi_name == auth.pi_name \
@@ -22,7 +22,5 @@ def authenticate(request):
 					return HttpResponse("OK!")
 				else:
 					return HttpResponse("Sign is currently in use. Please try again later.")
-		return HttpResponse("Not OK: pi_name: %s | uniq_id: %s" %(pi_name,uniq_id))
-		
-	else:
-		return HttpResponse("Something went wrong! Please re-scan the QR code!")
+	# Fallback respone
+	return HttpResponse("Something went wrong! Please re-scan the QR code!")
