49f182a882304d66f0423a0802a669cf3d1bbd09
Pheo <CJM1993@gmail.com>
2013-09-05 23:26:03 +1000

    Update QR.png

--- piServer/test.py
+++ piServer/test.py
@@ -1,4 +1,4 @@
 import QR
 from QR import generate_qrcode
 
-generate_qrcode('203.42.134.228/ads/authenticate/?pi_name=unimelb&uniq_id=1','QR.PNG')
+generate_qrcode('http://203.42.134.228/ads/authenticate/?pi_name=unimelb&uniq_id=1','QR.PNG')
