bf4bb497e0d24431e723d18298760a57a698bce8
Pheo <CJM1993@gmail.com>
2013-09-07 03:29:04 +1000

    Changed to mysql. Updated more models.

--- vmServer/ads/admin.py
+++ vmServer/ads/admin.py
@@ -1,9 +1,9 @@
 from django.contrib import admin
-from ads.models import Advertisment,Authentication, Disruption, ServiceLocation
+from ads.models import Advertisment, Authentication, Disruption, ServiceLocation, Weather, Location 
 
 admin.site.register(Advertisment)
 admin.site.register(Authentication)
 admin.site.register(Disruption)
 admin.site.register(ServiceLocation)
-
-	
+admin.site.register(Weather)
+admin.site.register(Location)
