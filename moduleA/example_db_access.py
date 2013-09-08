071fdc20e56e83eb44296e1bbb7d97b7febeb14c
Pheo <CJM1993@gmail.com>
2013-09-08 15:56:07 +1000

    updated example

--- moduleA/example_db_access.py
+++ moduleA/example_db_access.py
@@ -18,13 +18,28 @@ dbdet = "ids"
 db = MySQLdb.connect(host=hostdet,user=userdet,passwd=passwddet,db=dbdet)
 cur = db.cursor()
 
-# Standard MySQL query
-query = "SELECT * FROM Advertisment WHERE Name = 'Coca Cola'"
+# show tables in database 
+print "\nShow tables in ids database"
+query = "SHOW tables"
 cur.execute(query)
-
 # cur is a 'list' of rows. Hence the "for e in cur"
 for e in cur:
 	print e
+
+# show table schema 
+print "\nShow structure of ads_advertisment table"
+query = "DESCRIBE ads_advertisment"
+cur.execute(query)
+for e in cur:
+	print e
+
+# select rows from a table 
+print "\nSelect rows from ads_advertisment table"
+query = "SELECT * FROM ads_advertisment WHERE Name = 'Coca Cola'"
+cur.execute(query)
+for e in cur:
+	print e
+
 
 # Always remember to close. Don't leave hanging open conns
 cur.close()
