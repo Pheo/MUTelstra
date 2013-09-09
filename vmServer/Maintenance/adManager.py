7262544ac9202a52f1669096f8ed94110034cafb
Pheo <CJM1993@gmail.com>
2013-09-09 23:28:10 +1000

    Added moduleA dependents into Maintenance.

--- vmServer/Maintenance/adManager.py
+++ vmServer/Maintenance/adManager.py
@@ -6,30 +6,31 @@ import MySQLdb
 
 from weather import *
 
-hostdet = "localhost"
-userdet = "renlord"
-passwddet = "renlord"
-dbdet = "ids" 
-
-def connectDB():
-    'connects to the database in the Virtual Machine'
-    db = MySQLdb.connect(host=hostdet,user=userdet,passwd=passwddet,db=dbdet)
-    return db
-
-def closeDB(db):
-    'closes and commits all changes to the DB'
-    db.commit()
-    db.close()
 
 def updateAll():
 	db, cur = connectDB()
+	cur2 = db.cursor()
 	# iterate through the Weather Table, update weather for each location.
-
+	query = "SELECT LocID FROM ads_weather"
+	cur.execute(query)
+	for e in cur:
+		LocID = e[0]
+		query = "SELECT GPS_Lat, GPS_Long FROM ads_location WHERE LocID = %d" %(LocID)
+		cur2.execute(query)
+		for f in cur2:
+			GPS_Lat = f[0]
+			GPS_Long = f[1]
+			Weather, Param_List = getWeatherGPS(GPS_Lat, GPS_Long)
+			query = "UPDATE ads_weather SET Temp = %d, Cond = %d, Wind = %f,\
+				 Humidity = %d WHERE LocID = %d"\
+				 % (Weather['temp'], Weather['cond'], Weather['wind'],\
+					 Weather['humidity'], LocID)
 	return
 
 def pushAllSign():
 	# iterate through Weather Table, CALL push2sign(data) to push the 
 	# weather data to all signs with the location ID of the current entry.
+	
 
 	return
 
@@ -38,7 +39,7 @@ def push2sign(data, dest):
 	return
 
 def main():
-
+	updateAll()
 	return
 
 if __name__ == '__main__':
--- vmServer/ads/admin.py
+++ vmServer/ads/admin.py
@@ -1,7 +1,9 @@
 from django.contrib import admin
-from ads.models import Advertisment, Authentication, Weather, Location 
+from ads.models import Advertisment, Advertiser, Authentication, AdLocation, Weather, Location 
 
 admin.site.register(Advertisment)
+admin.site.register(Advertiser)
 admin.site.register(Authentication)
+admin.site.register(AdLocation)
 admin.site.register(Weather)
 admin.site.register(Location)
