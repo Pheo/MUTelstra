6aa14ffd9d5fcd3634c6b0b3e3c402c83203c0d5
Pheo <CJM1993@gmail.com>
2013-09-09 23:25:02 +1000

    Updated advertisments and added new advertisers table

--- vmServer/ads/models.py
+++ vmServer/ads/models.py
@@ -8,27 +8,49 @@ class Advertisment(models.Model):
 	Path	= models.CharField(max_length=256)
 	InDate	= models.DateTimeField('Date of Insertion')
 	ExpDate	= models.DateTimeField('Date of Expiry')
-	DTemp	= models.IntegerField()
+	DTemp	= models.IntegerField(null=True)
 	CHOICES_Cond = ((0,'MIST'),(1,'CLOUDS'),(2,'CLEAR'),(3,'RAIN'))
-	DCond	= models.IntegerField(choices=CHOICES_Cond)
-	DWind	= models.FloatField()
-	DHumidity = models.IntegerField()
-	DTime	= models.TimeField()
+	DCond	= models.IntegerField(choices=CHOICES_Cond, null=True)
+	DWind	= models.FloatField(null=True)
+	DHumidity = models.IntegerField(null=True)
+	DTime	= models.TimeField(null=True)
 	BidAmt	= models.FloatField()
 	MaxBdgt	= models.FloatField()
 	RelVar	= models.IntegerField()
 	Rel	= models.IntegerField(default=0)
+	RelMin	= models.IntegerField()
+	AdTime	= models.IntegerField()
+	MinIter	= models.IntegerField()
+	MaxIter	= models.IntegerField()
+	AdvertiserID = models.IntegerField()
 
 	def __unicode__(self):
 		return self.Name
+
+class Advertiser(models.Model):
+	AdvertiserID = models.AutoField(primary_key=True)
+	Username     = models.CharField(max_length=256)
+	Password     = models.CharField(max_length=256)
+	MaxAllowance = models.FloatField()
+	CurrentUsage = models.FloatField()
+	BillingDate  = models.FloatField()
+	Penalties    = models.FloatField()
+
 
 class Authentication(models.Model):
 	pi_name	= models.CharField(max_length=100,primary_key=True)
 	in_use	= models.BooleanField(default=False)
 	uniq_id	= models.CharField(max_length=100)
+	LocID	= models.IntegerField()
+	IPAddr	= models.CharField(max_length=100)
 
 	def __unicode__(self):
 		return self.pi_name
+
+class AdLocation (models.Model):
+	AdLocID	= models.AutoField(primary_key=True)
+	pi_name = models.CharField(max_length=100)
+	AdID	= models.IntegerField()	
 
 class Weather(models.Model):
 	LocID		= models.IntegerField(primary_key=True)
@@ -43,3 +65,4 @@ class Location(models.Model):
 	CityID	= models.IntegerField()
 	GPS_Lat = models.FloatField()
 	GPS_Long= models.FloatField()
+	Name	= models.CharField(max_length=100)
